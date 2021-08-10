ca9f3d89c89569b910b5986725e887387c99ab1b
shuvo18103107 <18103107@iubat.edu>
2021-08-10 17:55:12 +0600

    ✅ selecting and inserting DOM

--- script.js
+++ script.js
@@ -32,4 +32,50 @@ document.addEventListener('keydown', function (e) {
     }
 });
 
+//selecting elements
+//for this type of eleemnt we dont need any selector
+console.log(document);//only document is not proper dom element
+console.log(document.documentElement);//entire element select
+console.log(document.body);//entire body select
+console.log(document.head);//entire header selec
 
+const header = document.querySelector('.header');
+console.log(document.querySelectorAll('.header'));//return a nodeList
+document.querySelector('.header')
+const allSection = document.querySelectorAll('.section');
+console.log(allSection);
+console.log(document.getElementById('section--1'));
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);// this return a htmlcollection , if dom changes its will be update live
+console.log(document.getElementsByClassName('btn'));// aslo htmlcollection
+
+//creating and inserting element
+//  insertAdjacentHTML
+
+const message = document.createElement('div'); //create a dom element
+console.log(message);
+//as message now become a dom object so we can use it 
+message.classList.add('cookie-message');
+// message.textContent = 'WE use cookies for improved functionality and analytics';
+//textcontent dile html element count hobe na
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class ="btn btn--close--cookie">Got it!</button>';
+//prepend and append not only insert elemnt also move element
+//add element as a first child - prepend
+// header.prepend(message)
+// header.append(message)
+//1st prepend that append so element will be updated and visible one time in append way
+//if we want to change on both side then we have to amke a copy
+// header.append(message.cloneNode(true))
+header.append(message)
+//append - maek last child prepend make first child of an eleemnt but before and after set element before , after
+// but  live updated one will always visible , others are not working
+// header.before(message)
+// header.after(message)
+
+//delete elements
+
+document.querySelector('.btn--close--cookie').addEventListener('click', function () {
+    message.remove()
+    //older version of doing this
+    // message.parentElement.removeChild(message)
+})
