f2a277c8cfb45aaea7ff3d1c0f0ca1623ae6fab2
shuvo18103107 <18103107@iubat.edu>
2021-08-10 20:43:29 +0600

    ✅ smooth scrooling

---
+++ js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+    //page is jump when open modal
+    e.preventDefault();
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+
+//node;ist html dom array style e dekhai tai same class thakle r same functionality chaile forEach use kora better
+btnsOpenModal.forEach((v) => v.addEventListener('click', openModal))
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
+//smooth scrooling in JS
+const section1 = document.querySelector('#section--1');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+btnScrollTo.addEventListener('click', function () {
+    section1.scrollIntoView({ behavior: "smooth" })
+})
