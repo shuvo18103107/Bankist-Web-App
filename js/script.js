2fac9374d679f0a8ec8aefc40c456a804defa483
shuvo18103107 <18103107@iubat.edu>
2021-08-11 01:00:15 +0600

    🚑 implementing page navigation using event delegation

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const section1 = document.querySelector('#section--1');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
 
 const openModal = function (e) {
     //page is jump when open modal
@@ -21,10 +23,9 @@ const closeModal = function () {
     overlay.classList.add('hidden');
 };
 
-
 //node;ist html dom array style e dekhai tai same class thakle r same functionality chaile forEach use kora better
-btnsOpenModal.forEach((v) => v.addEventListener('click', openModal))
-btnCloseModal.addEventListener('click', closeModal)
+btnsOpenModal.forEach(v => v.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
@@ -32,10 +33,54 @@ document.addEventListener('keydown', function (e) {
     }
 });
 
-//smooth scrooling in JS
-const section1 = document.querySelector('#section--1');
-const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+//navigation
+//event delegation-- implementing page navigation
+
+//old way 
+// document.querySelectorAll('.nav__link').forEach(function (v) {
+//     v.addEventListener('click', function (e) {
+//         //prevent deafult anchor switching
+//         e.preventDefault();
+//         console.log('LINK');
+//         const id = this.getAttribute('href');
+//         document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+//         console.log(id);
+
+//         //so this call back function we put in 3 eleemnt but if there are thousands eleemnt and the same event for all of this then there might be a performance issue
+//         //this prb can be solved by eventdelegation - use the fact event bubble up - we do this by putting the eventListner to commom parent 
+//         //in our case we put eventhandler to parent ul and inside the ul all li when a user click it will follow the bubble up then we can catch the event in common parent element and handel it there 
+
+
+//     })
+// })
+
+// modern way implementing navigation using event delegation
+// 1. add eventlistner to common parent element
+//2. determine what element originate the event
+//3. mathching strategy
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    // now we have to figure out where this event actually happen , that is stored in e.target
+    // child e click korle parent e gelo thn search kore kothai event ta hoice 
+    e.preventDefault() // default event trigger off 
+    console.log(e.target);// targeted event store position
+
+    //matching strategy , mane common parent er event er response bad dibo
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+        console.log(id);
+    }
+    //sum: feature e click korle parent node ul e jabe tokhn check dibe event target konta sei event target e giye event function perform korbe , common parent er deafult behaviour off kore kaj korte hobe onlu child element gula select kore
+    //same event handler multiple place e bosanor theke event delegation lot better
+    // some element that not exist on run time but at the end it will visible it can be done by event delegation
+})
+
+//smooth scrooling in JS //button scrooling
 
 btnScrollTo.addEventListener('click', function () {
-    section1.scrollIntoView({ behavior: "smooth" })
-})
+    section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
