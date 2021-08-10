12378772e8bb32bb5de96255cd2027a91e8f1db3
shuvo18103107 <18103107@iubat.edu>
2021-08-10 11:53:48 +0600

    ✅ modal nodelist way execution

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+    //page is jump when open modal
+    e.preventDefault();
+
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
@@ -18,14 +21,15 @@ const closeModal = function () {
     overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', closeModal);
+//node;ist html dom array style e dekhai tai same class thakle r same functionality chaile forEach use kora better
+btnsOpenModal.forEach((v) => v.addEventListener('click', openModal))
+btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal);
-
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
     }
 });
+
+
